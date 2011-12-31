endlessm/xf86-video-intel

commit eb859f644633ee716083d253a5b7ff95163380e5

uxa/video: Correct the offset of the binding table in the surface buffer
The binding table is intended to be after all the surface descriptions,
so make sure we write it with the appropriate offset into the buffer.

Fixes regression from 699888a64 (uxa/video: Use the common bo
allocations and upload)

Reported-by: Cyril Brulebois <kibi@debian.org>
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=43704
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/i965_video.c
@@ -551,17 +551,19 @@ static void gen7_create_src_surface_state(ScrnInfoPtr scrn,
 }
 
 static void i965_create_binding_table(ScrnInfoPtr scrn,
-				drm_intel_bo *bind_bo,
-				int n_surf)
+				      drm_intel_bo *bind_bo,
+				      int n_surf)
 {
-	uint32_t binding_table[8];
+	uint32_t binding_table[n_surf];
 	int i;
 
 	/* Set up a binding table for our surfaces.  Only the PS will use it */
 	for (i = 0; i < n_surf; i++)
 		binding_table[i] = i * SURFACE_STATE_PADDED_SIZE;
 
-	dri_bo_subdata(bind_bo, 0, sizeof(uint32_t)*n_surf, binding_table);
+	dri_bo_subdata(bind_bo,
+		       n_surf * SURFACE_STATE_PADDED_SIZE,
+		       sizeof(binding_table), binding_table);
 }
 
 static drm_intel_bo *i965_create_sampler_state(ScrnInfoPtr scrn)

commit 770a953ff03bb8328c3f29e274d225528840f30c

uxa/video: Clear all state structures before uploading
A regression from eb859f644633e left some of the state uninitialised
before uploading to the GPU leading to undefined behaviour.

Reported-by: Alexey Shumitsky <alexey.shumitsky@gmail.com>
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=44338
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=44252
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/i965_video.c
@@ -588,6 +588,7 @@ static drm_intel_bo *gen7_create_sampler_state(ScrnInfoPtr scrn)
 	intel_screen_private *intel = intel_get_screen_private(scrn);
 	struct gen7_sampler_state sampler_state;
 
+	memset(&sampler_state, 0, sizeof(sampler_state));
 	sampler_state.ss0.min_filter = BRW_MAPFILTER_LINEAR;
 	sampler_state.ss0.mag_filter = BRW_MAPFILTER_LINEAR;
 	sampler_state.ss3.r_wrap_mode = BRW_TEXCOORDMODE_CLAMP;
@@ -605,6 +606,7 @@ static drm_intel_bo *i965_create_vs_state(ScrnInfoPtr scrn)
 	struct brw_vs_unit_state vs_state;
 
 	/* Set up the vertex shader to be disabled (passthrough) */
+	memset(&vs_state, 0, sizeof(vs_state));
 	if (IS_GEN5(intel))
 		vs_state.thread4.nr_urb_entries = URB_VS_ENTRIES >> 2;
 	else
@@ -794,6 +796,7 @@ static drm_intel_bo *i965_create_cc_vp_state(ScrnInfoPtr scrn)
 	intel_screen_private *intel = intel_get_screen_private(scrn);
 	struct brw_cc_viewport cc_viewport;
 
+	memset(&cc_viewport, 0, sizeof(cc_viewport));
 	cc_viewport.min_depth = -1.e35;
 	cc_viewport.max_depth = 1.e35;
 
@@ -1360,6 +1363,7 @@ gen6_create_cc_state(ScrnInfoPtr scrn)
 	intel_screen_private *intel = intel_get_screen_private(scrn);
 	struct gen6_color_calc_state cc_state;
 
+	memset(&cc_state, 0, sizeof(cc_state));
 	cc_state.constant_r = 1.0;
 	cc_state.constant_g = 0.0;
 	cc_state.constant_b = 1.0;
@@ -1376,6 +1380,7 @@ gen6_create_blend_state(ScrnInfoPtr scrn)
 	intel_screen_private *intel = intel_get_screen_private(scrn);
 	struct gen6_blend_state blend_state;
 
+	memset(&blend_state, 0, sizeof(blend_state));
 	blend_state.blend1.logic_op_enable = 1;
 	blend_state.blend1.logic_op_func = 0xc;
 	blend_state.blend1.pre_blend_clamp_enable = 1;