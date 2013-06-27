b5e85e495e55e2537d305b7bebacdf6f97b66199
Roy.Li <rongqing.li@windriver.com>
2013-06-27 14:10:14 +0800

    uxa: fix the compilation error with xorg-xserver <= 1.10
    struct _Screen has no canDoBGNoneRoot when ABI_VIDEODRV_VERSION is less than 10.0
    
    Signed-off-by: Roy.Li <rongqing.li@windriver.com>

--- src/intel_display.c
+++ src/intel_display.c
@@ -2113,7 +2113,9 @@ void intel_copy_fb(ScrnInfoPtr scrn)
 				0, 0,
 				scrn->virtualX, scrn->virtualY);
 	intel->uxa_driver->done_copy(dst);
+#if ABI_VIDEODRV_VERSION >= SET_ABI_VERSION(10, 0)
 	pScreen->canDoBGNoneRoot = TRUE;
+#endif
 
 cleanup_dst:
 	(*pScreen->DestroyPixmap)(dst);
