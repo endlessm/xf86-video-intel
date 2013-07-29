endlessm/xf86-video-intel

commit 6921abd81017c9ed7f3b2413784068fbc609a0ea

sna: Add a fast path for the most common fallback for CPU-CPU blits
This path will mostly be upload for individual glyph uploads, for which
the malloc overhead is significant.

Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/sna/sna_composite.c
@@ -470,6 +470,7 @@ sna_composite_fb(CARD8 op,
 	int src_xoff, src_yoff;
 	int msk_xoff, msk_yoff;
 	int dst_xoff, dst_yoff;
+	int16_t tx, ty;
 	unsigned flags;
 
 	DBG(("%s -- op=%d, fallback dst=(%d, %d)+(%d, %d), size=(%d, %d): region=((%d,%d), (%d, %d))\n",
@@ -518,6 +519,44 @@ sna_composite_fb(CARD8 op,
 	validate_source(src);
 	if (mask)
 		validate_source(mask);
+
+	if (mask == NULL &&
+	    src->pDrawable &&
+	    src->filter != PictFilterConvolution &&
+	    (op == PictOpSrc || (op == PictOpOver && !PICT_FORMAT_A(src->format))) &&
+	    (dst->format == src->format || dst->format == alphaless(src->format)) &&
+	    sna_transform_is_integer_translation(src->transform, &tx, &ty) &&
+	    region->extents.x1 + src_x + tx >= 0 &&
+	    region->extents.y1 + src_y + ty >= 0 &&
+	    region->extents.x2 + src_x + tx <= src->pDrawable->width &&
+	    region->extents.x2 + src_y + ty <= src->pDrawable->height) {
+		PixmapPtr dst_pixmap = get_drawable_pixmap(dst->pDrawable);
+		PixmapPtr src_pixmap = get_drawable_pixmap(src->pDrawable);
+		int nbox = RegionNumRects(region);
+		BoxPtr box = RegionRects(region);
+
+		src_x += tx; src_y += ty;
+
+		get_drawable_deltas(src->pDrawable, src_pixmap, &tx, &ty);
+		src_x += tx; src_y += ty;
+
+		get_drawable_deltas(dst->pDrawable, dst_pixmap, &tx, &ty);
+		dst_x += tx; dst_y += ty;
+
+		do {
+			memcpy_blt(src_pixmap->devPrivate.ptr,
+				   dst_pixmap->devPrivate.ptr,
+				   dst_pixmap->drawable.bitsPerPixel,
+				   src_pixmap->devKind,
+				   dst_pixmap->devKind,
+				   box->x1 + src_x, box->y1 + src_y,
+				   box->x1 + dst_x, box->y1 + dst_y,
+				   box->x2 - box->x1, box->y2 - box->y1);
+			box++;
+		} while (--nbox);
+
+		return;
+	}
 
 	src_image = image_from_pict(src, FALSE, &src_xoff, &src_yoff);
 	mask_image = image_from_pict(mask, FALSE, &msk_xoff, &msk_yoff);

src/sna/sna_blt.c
@@ -1953,13 +1953,6 @@ prepare_blt_put(struct sna *sna,
 	return true;
 }
 
-#define alphaless(format) PICT_FORMAT(PICT_FORMAT_BPP(format),		\
-				      PICT_FORMAT_TYPE(format),		\
-				      0,				\
-				      PICT_FORMAT_R(format),		\
-				      PICT_FORMAT_G(format),		\
-				      PICT_FORMAT_B(format))
-
 static bool
 is_clear(PixmapPtr pixmap)
 {

src/sna/sna_render.h
@@ -775,4 +775,11 @@ static inline bool sna_vertex_wait__locked(struct sna_render *r)
 	return was_active;
 }
 
+#define alphaless(format) PICT_FORMAT(PICT_FORMAT_BPP(format),		\
+				      PICT_FORMAT_TYPE(format),		\
+				      0,				\
+				      PICT_FORMAT_R(format),		\
+				      PICT_FORMAT_G(format),		\
+				      PICT_FORMAT_B(format))
+
 #endif /* SNA_RENDER_H */

commit c9d89499806779cd6c62d5d6d34df76279cc5abd

sna: Composite region is already in dst drawable space
So do not offset it again when processing the fallback composite
operation.

Regression from commit 6921abd81017c9ed7f3b2413784068fbc609a0ea
Author: Chris Wilson <chris@chris-wilson.co.uk>
Date:   Thu Jul 18 16:21:27 2013 +0100

    sna: Add a fast path for the most common fallback for CPU-CPU blits

References: https://bugs.freedesktop.org/show_bug.cgi?id=66990
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/sna/sna_composite.c
@@ -529,7 +529,8 @@ sna_composite_fb(CARD8 op,
 	    sna_transform_is_integer_translation(src->transform, &tx, &ty)) {
 		PixmapPtr dst_pixmap = get_drawable_pixmap(dst->pDrawable);
 		PixmapPtr src_pixmap = get_drawable_pixmap(src->pDrawable);
-		int16_t sx = src_x + tx, sy = src_y + ty;
+		int16_t sx = src_x + tx - (dst->pDrawable->x - dst_x);
+		int16_t sy = src_y + ty - (dst->pDrawable->y - dst_y);
 		if (region->extents.x1 + sx >= 0 &&
 		    region->extents.y1 + sy >= 0 &&
 		    region->extents.x2 + sx <= src->pDrawable->width &&
@@ -547,8 +548,6 @@ sna_composite_fb(CARD8 op,
 			assert(region->extents.y1 + sy >= 0);
 			assert(region->extents.y2 + sy <= src_pixmap->drawable.height);
 
-			dst_x += dst->pDrawable->x;
-			dst_y += dst->pDrawable->y;
 			if (get_drawable_deltas(dst->pDrawable, dst_pixmap, &tx, &ty))
 				dst_x += tx, dst_y += ty;
 