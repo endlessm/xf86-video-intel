0e1abe3cd01e5ea1fcb38c628d3ef81ecc0dae97
Chris Wilson <chris@chris-wilson.co.uk>
2013-09-19 12:07:04 +0100

    sna/gen4: Remove long dead code for forced batch emission between CA passes
    Forcing the batch emission on virtually every glyph eats a lot of CPU
    time sending very short commands to the GPU, and is totally unnecessary.
    
    Reported-by: Arkadiusz Miskiewicz <a.miskiewicz@gmail.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/sna/gen4_render.c
+++ src/sna/gen4_render.c
@@ -632,9 +632,6 @@ static int gen4_get_rectangles__flush(struct sna *sna,
 	if (!kgem_check_reloc_and_exec(&sna->kgem, 2))
 		return 0;
 
-	if (op->need_magic_ca_pass && sna->render.vbo)
-		return 0;
-
 	if (sna->render.vertex_offset) {
 		gen4_vertex_flush(sna);
 		if (gen4_magic_ca_pass(sna, op))
