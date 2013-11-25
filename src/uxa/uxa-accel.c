00dcb4f22942b2d40253a7b43105e73e260216cf
Zdenek Kabelac <zkabelac@redhat.com>
2013-11-25 09:09:35 +0100

    uxa: fix order of inline Bool
    Signed-off-by: Zdenek Kabelac <zkabelac@redhat.com>

--- src/uxa/uxa-accel.c
+++ src/uxa/uxa-accel.c
@@ -235,7 +235,7 @@ fallback:
 	}
 }
 
-static Bool inline
+static inline Bool
 uxa_copy_n_to_n_two_dir(DrawablePtr pSrcDrawable, DrawablePtr pDstDrawable,
 			GCPtr pGC, BoxPtr pbox, int nbox, int dx, int dy)
 {
