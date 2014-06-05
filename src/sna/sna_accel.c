endlessm/xf86-video-intel

commit 65301412ecf2d55ab55a2d7faeaa048d4ee8b1d0

sna: Discard active GPU buffers before uploading into them
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/sna/sna_accel.c
@@ -4491,8 +4491,12 @@ try_upload_tiled_x(PixmapPtr pixmap, RegionRec *region,
 	}
 
 	if (priv->gpu_bo && replaces) {
-		DBG(("%s: discarding cached upload proxy\n", __FUNCTION__));
-		sna_pixmap_free_gpu(sna, priv);
+		if (UNDO)
+			kgem_bo_undo(&sna->kgem, priv->gpu_bo);
+		if (__kgem_bo_is_busy(&sna->kgem, priv->gpu_bo)) {
+			DBG(("%s: discarding cached upload proxy\n", __FUNCTION__));
+			sna_pixmap_free_gpu(sna, priv);
+		}
 		replaces = true; /* Mark it all GPU damaged afterwards */
 	}
 	assert(priv->gpu_bo == NULL || priv->gpu_bo->proxy == NULL);
@@ -4501,6 +4505,17 @@ try_upload_tiled_x(PixmapPtr pixmap, RegionRec *region,
 	    (!replaces || !sna_pixmap_move_to_gpu(pixmap, MOVE_WRITE))) {
 		DBG(("%s: no, has COW or pending move-to-gpu\n", __FUNCTION__));
 		return false;
+	}
+
+	if (priv->gpu_damage &&
+	    region_subsumes_damage(region, priv->gpu_damage)) {
+		if (UNDO)
+			kgem_bo_undo(&sna->kgem, priv->gpu_bo);
+		if (__kgem_bo_is_busy(&sna->kgem, priv->gpu_bo)) {
+			DBG(("%s: discarding dirty pixmap\n", __FUNCTION__));
+			sna_pixmap_free_gpu(sna, priv);
+		}
+		replaces = true; /* Mark it all GPU damaged afterwards */
 	}
 
 	if (priv->gpu_bo == NULL &&

commit 1327918f91dce2051b809594db5a3ed555934612

sna: Mark CPU as all damaged when discarding a busy GPU bo
When we discard a GPU bo, we need to make sure that the remaining
content is marked as only accessible via the CPU shadow pointer.

Regression from commit 65301412ecf2d55ab55a2d7faeaa048d4ee8b1d0
Author: Chris Wilson <chris@chris-wilson.co.uk>
Date:   Sat May 17 20:59:38 2014 +0100

    sna: Discard active GPU buffers before uploading into them

Reported-by: Jan Alexander Steffens <jan.steffens@gmail.com>
Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=79517
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/sna/sna_accel.c
@@ -4602,6 +4602,10 @@ try_upload_tiled_x(PixmapPtr pixmap, RegionRec *region,
 			     kgem_bo_can_map__cpu(&sna->kgem, priv->gpu_bo, true)));
 			sna_pixmap_free_gpu(sna, priv);
 			ignore_cpu = priv->cpu_damage == NULL;
+			if (priv->ptr)
+				sna_damage_all(&priv->cpu_damage,
+						pixmap->drawable.width,
+						pixmap->drawable.height);
 		}
 	}
 