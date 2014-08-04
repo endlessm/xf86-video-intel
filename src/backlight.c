endlessm/xf86-video-intel

commit f9e7ac7db7b0331131aa1df3a90d4b2692949efa

backlight: Set structure to safe values when not initialised
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/backlight.c
@@ -74,6 +74,15 @@
  * If only things were as simple as on OpenBSD! :)
  */
 
+void backlight_init(struct backlight *b)
+{
+	b->type = BL_NONE;
+	b->iface = NULL;
+	b->fd = -1;
+	b->pid = -1;
+	b->max = -1;
+}
+
 #ifdef __OpenBSD__
 
 #include <dev/wscons/wsconsio.h>
@@ -384,23 +393,27 @@ int backlight_open(struct backlight *b, char *iface)
 	if (iface == NULL)
 		iface = __backlight_find();
 	if (iface == NULL)
-		return -1;
+		goto err;
 
 	b->type = __backlight_type(iface);
 
 	b->max = __backlight_read(iface, "max_brightness");
 	if (b->max <= 0)
-		return -1;
+		goto err;
 
 	level = __backlight_read(iface, "brightness");
-	if (level < 0)
-		return -1;
+	if (level)
+		goto err;
 
 	if (!__backlight_direct_init(b, iface) &&
 	    !__backlight_helper_init(b, iface))
-		return -1;
+		goto err;
 
 	return level;
+
+err:
+	backlight_init(b);
+	return -1;
 }
 
 int backlight_set(struct backlight *b, int level)

src/backlight.h
@@ -38,12 +38,13 @@ enum backlight_type {
 struct backlight {
 	char *iface;
 	enum backlight_type type;
-	int max;
+	int original, max;
 	int pid, fd;
 };
 
 enum backlight_type backlight_exists(const char *iface);
 
+void backlight_init(struct backlight *backlight);
 int backlight_open(struct backlight *backlight, char *iface);
 int backlight_set(struct backlight *backlight, int level);
 int backlight_get(struct backlight *backlight);

src/sna/sna_display.c
@@ -3683,6 +3683,7 @@ reset:
 	output->driver_private = sna_output;
 	sna_output->base = output;
 
+	backlight_init(&sna_output->backlight);
 	if (sna_output->is_panel)
 		sna_output_backlight_init(output);
 

commit ebd1907304c538bcc27614bc8e2e29f317ffb390

backlight: Fix regression
The backlight xrandr property is missing unless the backlight level
was 0 on starting X, regression introduced in

commit f9e7ac7db7b0331131aa1df3a90d4b2692949efa
Author: Chris Wilson <chris@chris-wilson.co.uk>
Date:   Wed Jul 23 21:53:31 2014 +0100

    backlight: Set structure to safe values when not initialised

Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=82146

src/backlight.c
@@ -402,7 +402,7 @@ int backlight_open(struct backlight *b, char *iface)
 		goto err;
 
 	level = __backlight_read(iface, "brightness");
-	if (level)
+	if (level < 0)
 		goto err;
 
 	if (!__backlight_direct_init(b, iface) &&