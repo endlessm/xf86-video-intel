d2b7d78064ca224bdcc365bede1b16991d43caa8
Chris Wilson <chris@chris-wilson.co.uk>
2015-01-26 21:30:44 +0000

    sna/dri2: Use existing busy state if semaphores are disabled
    Undo d16ac1f30bb (sna/dri2: Prefer to use normal selection criteria for
    CopyRegion on small GT) to keep on the current ring as the external bo
    if semaphores are disabled.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- src/sna/sna_dri2.c
+++ src/sna/sna_dri2.c
@@ -841,7 +841,7 @@ static void sna_dri2_select_mode(struct sna *sna, struct kgem_bo *dst, struct kg
 		return;
 	}
 
-	if (sna->render_state.gt < 2) {
+	if (sna->render_state.gt < 2 && sna->kgem.has_semaphores) {
 		DBG(("%s: small GT [%d], not forcing selection\n",
 		     __FUNCTION__, sna->render_state.gt));
 		return;
