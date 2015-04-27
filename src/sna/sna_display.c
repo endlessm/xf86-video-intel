747c5475f770170aee7d6d14924ec8f6cc595f4d
Jasper St. Pierre <jstpierre@mecheye.net>
2015-04-27 15:40:27 -0700

    sna_display: Fix copy of shadowfb contents
    When we enable overscan, we need to include the overscan border when we
    copy over the contents of shadowfb during resolution switches.
    Otherwise, we'll get junk contents outside of the overscan border.

--- src/sna/sna_display.c
+++ src/sna/sna_display.c
@@ -2635,7 +2635,7 @@ force_shadow:
 
 				if (!sna->render.copy_boxes(sna, GXcopy,
 							     &front->drawable, __sna_pixmap_get_bo(front), 0, 0,
-							     &tmp, bo, -crtc->x, -crtc->y,
+							     &tmp, bo, -crtc->x + xu, -crtc->y + yu,
 							     &b, 1, COPY_LAST))
 					shadow_clear(sna, front, bo, crtc);
 			}
